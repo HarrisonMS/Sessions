8be6ceede411cd6276589e235ed741ea9d901cac
Harrison <hmseaborn@gmail.com>
2020-05-18 13:23:48 -0700

    server is up and running
    had an issue i was exporting my session config as a object when i did not need to

--- api/serverConfig.js
+++ api/serverConfig.js
@@ -2,12 +2,22 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
-const sessions = require("express-session");
-const KnexSessionStore = require("connect-session-knex")(sessions);
+const session = require("express-session");
 const sessionConfig = require("./sessionConfig");
 
+// const sessionConfig = {
+//   name: "monster",
+//   secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
+//   resave: false,
+//   saveUninitialized: process.env.SEND_COOKIES || true,
+//   cookie: {
+//     maxAge: 1000 * 60 * 10, // good for 10 mins in ms
+//     secure: process.env.USE_SECURE_COOKIES || false, // used over https only, set to true in production
+//     httpOnly: true, // true means JS on the client cannot access the cooke
+//   },
+// };
 module.exports = (server) => {
-  server.use(sessions(sessionConfig));
+  server.use(session(sessionConfig));
   server.use(morgan("dev"));
   server.use(helmet());
   server.use(express.json());
--- api/sessionConfig.js
+++ api/sessionConfig.js
@@ -3,14 +3,6 @@ const sessionConfig = {
   secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
   resave: false,
   saveUninitialized: process.env.SEND_COOKIES || true,
-  store: new KnexSessionStore({
-    knex,
-    createtable: true,
-
-    clearInterval: 1000 * 50 * 10,
-    sidfieldname: "sid",
-    tablename: "auth",
-  }),
   cookie: {
     maxAge: 1000 * 60 * 10, // good for 10 mins in ms
     secure: process.env.USE_SECURE_COOKIES || false, // used over https only, set to true in production
@@ -18,6 +10,4 @@ const sessionConfig = {
   },
 };
 
-module.exports = {
-  sessionConfig: sessionConfig,
-};
+module.exports = sessionConfig;
